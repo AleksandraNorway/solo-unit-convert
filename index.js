a1f6fa4f83c5120c10f39abeba5fbbe554d34b2c
AleksandraNorway <phimaster.ai@gmail.com>
2024-02-02 10:15:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,28 +5,35 @@
 */
 
 let btnConvert = document.getElementById("btn")
-let inputAmmount = document.getElementById("input-numer")
+let inputAmount = document.getElementById("input-numer")
+
 
 let length = document.getElementById("length")
 let volume = document.getElementById("volume")
 let mass = document.getElementById("mass")
 
+
 btnConvert.addEventListener("click", function() {
+      // Check for negative input
+      if (inputAmount.value < 1) {
+        alert("Input value must not be negative.");
+        return;
+    }
     /* 1 meter = 3.281 feet */
-    let feet = inputAmmount.value * 3.281
-    let meter = inputAmmount.value / 3.281
+    let feet = inputAmount.value * 3.281
+    let meter = inputAmount.value / 3.281
     
     /* 1 liter = 0.264 gallon */
-    let gallon = inputAmmount.value * 0.264
-    let liter = inputAmmount.value / 0.264
+    let gallon = inputAmount.value * 0.264
+    let liter = inputAmount.value / 0.264
     
     /* 1 kilogram = 2.204 pound */
-    let pound = inputAmmount.value * 2.204
-    let kilogram = inputAmmount.value / 2.204
+    let pound = inputAmount.value * 2.204
+    let kilogram = inputAmount.value / 2.204
     
-    length.innerHTML = `${inputAmmount.value} meters = ${feet.toFixed(3)} feet | ${inputAmmount.value} feet = ${meter.toFixed(3)} meters`
+    length.innerHTML = `${inputAmount.value} meters = ${feet.toFixed(3)} feet | ${inputAmount.value} feet = ${meter.toFixed(3)} meters`
     
-    volume.innerHTML = `${inputAmmount.value} liters = ${gallon.toFixed(3)} gallons | ${inputAmmount.value} gallons = ${meter.toFixed(3)} liters`
+    volume.innerHTML = `${inputAmount.value} liters = ${gallon.toFixed(3)} gallons | ${inputAmount.value} gallons = ${meter.toFixed(3)} liters`
     
-    mass.innerHTML = `${inputAmmount.value} kilos = ${pound.toFixed(3)} pounds | ${inputAmmount.value}  pounds = ${kilogram.toFixed(3)} kilos`
+    mass.innerHTML = `${inputAmount.value} kilos = ${pound.toFixed(3)} pounds | ${inputAmount.value}  pounds = ${kilogram.toFixed(3)} kilos`
 })
